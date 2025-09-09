0fe3c63528a761c0572f4550124ac33677ebd830
Mike van Oorschot <mikevanoo@gmail.com>
2025-09-09 14:23:10 +0100

    Fix compiler warnings.

--- src/DotNetDistributedApp.Api/Clients/GeoIpResponseDto.cs
+++ src/DotNetDistributedApp.Api/Clients/GeoIpResponseDto.cs
@@ -2,13 +2,13 @@
 
 public class GeoIpResponseDto
 {
-    public string Country { get; set; }
-    public string Latitude { get; set; }
-    public string Longitude { get; set; }
-    public string Continent { get; set; }
-    public string Timezone { get; set; }
+    public required string Country { get; set; }
+    public required string Latitude { get; set; }
+    public required string Longitude { get; set; }
+    public required string Continent { get; set; }
+    public required string Timezone { get; set; }
     public int AccuracyRadius { get; set; }
     public int Asn { get; set; }
-    public string AsnOrganization { get; set; }
-    public string AsnNetwork { get; set; }
+    public required string AsnOrganization { get; set; }
+    public required string AsnNetwork { get; set; }
 }

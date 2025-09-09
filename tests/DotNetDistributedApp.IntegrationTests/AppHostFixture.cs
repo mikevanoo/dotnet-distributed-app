0b6fda4ee21f2ac3642646c5ff5ebd59b7090df1
Mike van Oorschot <mikevanoo@gmail.com>
2025-09-09 14:20:48 +0100

    Reinstate all integration tests.

--- tests/DotNetDistributedApp.IntegrationTests/AppHostFixture.cs
+++ tests/DotNetDistributedApp.IntegrationTests/AppHostFixture.cs
@@ -1,51 +1,51 @@
-﻿// using Aspire.Hosting;
-// using DotNetDistributedApp.IntegrationTests;
-// using Microsoft.Extensions.Logging;
-//
-// [assembly: AssemblyFixture(typeof(AppHostFixture))]
-//
-// namespace DotNetDistributedApp.IntegrationTests;
-//
-// public class AppHostFixture : IAsyncLifetime
-// {
-//     public static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
-//     public DistributedApplication App { get; private set; } = null!;
-//
-//     public async ValueTask InitializeAsync()
-//     {
-//         var cancellationToken = CreateCancellationToken();
-//         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.DotNetDistributedApp_AppHost>(
-//             cancellationToken
-//         );
-//         appHost.Services.AddLogging(logging =>
-//         {
-//             logging.SetMinimumLevel(LogLevel.Debug);
-//             // Override the logging filters from the app's configuration
-//             logging.AddFilter(appHost.Environment.ApplicationName, LogLevel.Debug);
-//             logging.AddFilter("Aspire.", LogLevel.Debug);
-//             // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
-//         });
-//         appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-//         {
-//             clientBuilder.AddStandardResilienceHandler();
-//         });
-//
-//         App = await appHost.BuildAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
-//         await App.StartAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
-//
-//         await App
-//             .ResourceNotifications.WaitForResourceHealthyAsync("api", cancellationToken)
-//             .WaitAsync(AppHostFixture.DefaultTimeout, cancellationToken);
-//     }
-//
-//     public static CancellationToken CreateCancellationToken(TimeSpan? timeout = null)
-//     {
-//         return new CancellationTokenSource(timeout ?? DefaultTimeout).Token;
-//     }
-//
-//     public async ValueTask DisposeAsync()
-//     {
-//         await App.DisposeAsync();
-//         GC.SuppressFinalize(this);
-//     }
-// }
+﻿using Aspire.Hosting;
+using DotNetDistributedApp.IntegrationTests;
+using Microsoft.Extensions.Logging;
+
+[assembly: AssemblyFixture(typeof(AppHostFixture))]
+
+namespace DotNetDistributedApp.IntegrationTests;
+
+public class AppHostFixture : IAsyncLifetime
+{
+    public static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
+    public DistributedApplication App { get; private set; } = null!;
+
+    public async ValueTask InitializeAsync()
+    {
+        var cancellationToken = CreateCancellationToken();
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.DotNetDistributedApp_AppHost>(
+            cancellationToken
+        );
+        appHost.Services.AddLogging(logging =>
+        {
+            logging.SetMinimumLevel(LogLevel.Debug);
+            // Override the logging filters from the app's configuration
+            logging.AddFilter(appHost.Environment.ApplicationName, LogLevel.Debug);
+            logging.AddFilter("Aspire.", LogLevel.Debug);
+            // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+        });
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+
+        App = await appHost.BuildAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+        await App.StartAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+
+        await App
+            .ResourceNotifications.WaitForResourceHealthyAsync("api", cancellationToken)
+            .WaitAsync(AppHostFixture.DefaultTimeout, cancellationToken);
+    }
+
+    public static CancellationToken CreateCancellationToken(TimeSpan? timeout = null)
+    {
+        return new CancellationTokenSource(timeout ?? DefaultTimeout).Token;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await App.DisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+}
--- tests/DotNetDistributedApp.IntegrationTests/SpatialApi/CoordinateConverterShould.cs
+++ tests/DotNetDistributedApp.IntegrationTests/SpatialApi/CoordinateConverterShould.cs
@@ -1,47 +1,47 @@
-// using AwesomeAssertions;
-// using DotNetDistributedApp.SpatialApi.CoordinateConverter;
-//
-// namespace DotNetDistributedApp.IntegrationTests.SpatialApi;
-//
-// public class CoordinateConverterShould(AppHostFixture appHostFixture)
-// {
-//     [Fact]
-//     public async Task ToOsNationalGridReferenceReturn200OkWithData()
-//     {
-//         var httpClient = appHostFixture.App.CreateHttpClient("spatial-api");
-//
-//         var response = await httpClient.GetAsync(
-//             "/coordinate-converter/to-os-national-grid-reference?latitude=58.214&longitude=-6.318",
-//             AppHostFixture.CreateCancellationToken()
-//         );
-//
-//         response
-//             .Should()
-//             .Be200Ok()
-//             .And.Satisfy<OsNationalGridReferenceDto>(model =>
-//             {
-//                 model.Easting.Should().BePositive();
-//                 model.Northing.Should().BePositive();
-//             });
-//     }
-//
-//     [Fact]
-//     public async Task ToLatitudeLongitudeReturn200OkWithData()
-//     {
-//         var httpClient = appHostFixture.App.CreateHttpClient("spatial-api");
-//
-//         var response = await httpClient.GetAsync(
-//             "/coordinate-converter/to-latitude-longitude?easting=146400&northing=933200",
-//             AppHostFixture.CreateCancellationToken()
-//         );
-//
-//         response
-//             .Should()
-//             .Be200Ok()
-//             .And.Satisfy<LatitudeLongitudeDto>(model =>
-//             {
-//                 model.Latitude.Should().BePositive();
-//                 model.Longitude.Should().BeNegative();
-//             });
-//     }
-// }
+using AwesomeAssertions;
+using DotNetDistributedApp.SpatialApi.CoordinateConverter;
+
+namespace DotNetDistributedApp.IntegrationTests.SpatialApi;
+
+public class CoordinateConverterShould(AppHostFixture appHostFixture)
+{
+    [Fact]
+    public async Task ToOsNationalGridReferenceReturn200OkWithData()
+    {
+        var httpClient = appHostFixture.App.CreateHttpClient("spatial-api");
+
+        var response = await httpClient.GetAsync(
+            "/coordinate-converter/to-os-national-grid-reference?latitude=58.214&longitude=-6.318",
+            AppHostFixture.CreateCancellationToken()
+        );
+
+        response
+            .Should()
+            .Be200Ok()
+            .And.Satisfy<OsNationalGridReferenceDto>(model =>
+            {
+                model.Easting.Should().BePositive();
+                model.Northing.Should().BePositive();
+            });
+    }
+
+    [Fact]
+    public async Task ToLatitudeLongitudeReturn200OkWithData()
+    {
+        var httpClient = appHostFixture.App.CreateHttpClient("spatial-api");
+
+        var response = await httpClient.GetAsync(
+            "/coordinate-converter/to-latitude-longitude?easting=146400&northing=933200",
+            AppHostFixture.CreateCancellationToken()
+        );
+
+        response
+            .Should()
+            .Be200Ok()
+            .And.Satisfy<LatitudeLongitudeDto>(model =>
+            {
+                model.Latitude.Should().BePositive();
+                model.Longitude.Should().BeNegative();
+            });
+    }
+}

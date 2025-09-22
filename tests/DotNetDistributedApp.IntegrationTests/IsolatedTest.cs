e45c43aa95482cf88dc3301f8ea74390085a6d84
Mike van Oorschot <mikevanoo@gmail.com>
2025-09-22 08:37:43 +0100

    Running the Aspire integration tests via Rider seems to have fixed itself so commenting out these isolated tests as they interfere (they are mutually exclusive with all the other tests that use the AppHostFixture).

--- tests/DotNetDistributedApp.IntegrationTests/IsolatedTest.cs
+++ tests/DotNetDistributedApp.IntegrationTests/IsolatedTest.cs
@@ -8,49 +8,49 @@ namespace DotNetDistributedApp.IntegrationTests;
  * Comment out all other tests and AppHostFixture.cs and run via `dotnet test`.
  */
 
-public class IsolatedTest(ITestOutputHelper testOutputHelper)
-{
-    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromMinutes(2);
-
-    [Fact]
-    public async Task GetWebResourceRootReturnsOkStatusCode()
-    {
-        // Arrange
-        var cancellationToken = new CancellationTokenSource(DefaultTimeout).Token;
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.DotNetDistributedApp_AppHost>(
-            cancellationToken
-        );
-        appHost.Services.AddLogging(logging =>
-        {
-            logging.SetMinimumLevel(LogLevel.Debug);
-            logging.AddXUnit(testOutputHelper);
-            // Override the logging filters from the app's configuration
-            logging.AddFilter(appHost.Environment.ApplicationName, LogLevel.Debug);
-            logging.AddFilter("Aspire.", LogLevel.Debug);
-        });
-        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-        {
-            clientBuilder.AddStandardResilienceHandler();
-        });
-
-        await using var app = await appHost.BuildAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
-        await app.StartAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
-        await app
-            .ResourceNotifications.WaitForResourceHealthyAsync("api", cancellationToken)
-            .WaitAsync(DefaultTimeout, cancellationToken);
-
-        // Act
-        var httpClient = app.CreateHttpClient("spatial-api");
-        await app
-            .ResourceNotifications.WaitForResourceHealthyAsync("spatial-api", cancellationToken)
-            .WaitAsync(DefaultTimeout, cancellationToken);
-
-        var response = await httpClient.GetAsync(
-            "/coordinate-converter/to-os-national-grid-reference?latitude=58.214&longitude=-6.318",
-            cancellationToken
-        );
-
-        // Assert
-        response.Should().Be200Ok();
-    }
-}
+// public class IsolatedTest(ITestOutputHelper testOutputHelper)
+// {
+//     private static readonly TimeSpan DefaultTimeout = TimeSpan.FromMinutes(2);
+//
+//     [Fact]
+//     public async Task GetWebResourceRootReturnsOkStatusCode()
+//     {
+//         // Arrange
+//         var cancellationToken = new CancellationTokenSource(DefaultTimeout).Token;
+//         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.DotNetDistributedApp_AppHost>(
+//             cancellationToken
+//         );
+//         appHost.Services.AddLogging(logging =>
+//         {
+//             logging.SetMinimumLevel(LogLevel.Debug);
+//             logging.AddXUnit(testOutputHelper);
+//             // Override the logging filters from the app's configuration
+//             logging.AddFilter(appHost.Environment.ApplicationName, LogLevel.Debug);
+//             logging.AddFilter("Aspire.", LogLevel.Debug);
+//         });
+//         appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+//         {
+//             clientBuilder.AddStandardResilienceHandler();
+//         });
+//
+//         await using var app = await appHost.BuildAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+//         await app.StartAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+//         await app
+//             .ResourceNotifications.WaitForResourceHealthyAsync("api", cancellationToken)
+//             .WaitAsync(DefaultTimeout, cancellationToken);
+//
+//         // Act
+//         var httpClient = app.CreateHttpClient("spatial-api");
+//         await app
+//             .ResourceNotifications.WaitForResourceHealthyAsync("spatial-api", cancellationToken)
+//             .WaitAsync(DefaultTimeout, cancellationToken);
+//
+//         var response = await httpClient.GetAsync(
+//             "/coordinate-converter/to-os-national-grid-reference?latitude=58.214&longitude=-6.318",
+//             cancellationToken
+//         );
+//
+//         // Assert
+//         response.Should().Be200Ok();
+//     }
+// }

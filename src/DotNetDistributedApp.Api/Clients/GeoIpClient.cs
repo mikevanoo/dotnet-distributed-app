e62827c84d92bd75ee6ca519bc34d728f0a0c415
Mike van Oorschot <mikevanoo@gmail.com>
2026-03-09 09:27:20 +0000

    Improve `GeoIpClient` error handling by throwing `HttpRequestException` on non-success API responses.

--- src/DotNetDistributedApp.Api/Clients/GeoIpClient.cs
+++ src/DotNetDistributedApp.Api/Clients/GeoIpClient.cs
@@ -1,4 +1,4 @@
-﻿using DotNetDistributedApp.Api.Common.Metrics;
+using DotNetDistributedApp.Api.Common.Metrics;
 using Microsoft.Extensions.Caching.Hybrid;
 
 namespace DotNetDistributedApp.Api.Clients;
@@ -16,33 +16,42 @@ public partial class GeoIpClient(
         var cacheKey = $"{nameof(GeoIpClient)}:{ipAddress}";
         var cacheMiss = false;
 
-        var result = await cache.GetOrCreateAsync(
-            cacheKey,
-            async cancellationToken =>
-            {
-                cacheMiss = true;
-                LogCacheMiss(cacheKey, ipAddress);
-                metricsService.CacheMiss(1, cacheKey);
-
-                var response = await httpClient.GetAsync(url, cancellationToken);
-                if (!response.IsSuccessStatusCode)
-                {
-                    LogCouldNotGetGeoInformation(ipAddress);
-                    return null;
-                }
-
-                var result = await response.Content.ReadFromJsonAsync<GeoIpResponseDto>(cancellationToken);
-
-                return result;
-            }
-        );
-
-        if (!cacheMiss)
+        try
         {
-            metricsService.CacheHit(1, cacheKey);
-        }
+            var result = await cache.GetOrCreateAsync(
+                cacheKey,
+                async cancellationToken =>
+                {
+                    cacheMiss = true;
+                    LogCacheMiss(cacheKey, ipAddress);
+                    metricsService.CacheMiss(1, cacheKey);
 
-        return result;
+                    var response = await httpClient.GetAsync(url, cancellationToken);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        LogCouldNotGetGeoInformation(ipAddress);
+                        throw new HttpRequestException(
+                            $"GeoIP API returned {response.StatusCode} for IP address {ipAddress}",
+                            inner: null,
+                            response.StatusCode
+                        );
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<GeoIpResponseDto>(cancellationToken);
+                }
+            );
+
+            if (!cacheMiss)
+            {
+                metricsService.CacheHit(1, cacheKey);
+            }
+
+            return result;
+        }
+        catch (HttpRequestException)
+        {
+            return null;
+        }
     }
 
     [LoggerMessage(

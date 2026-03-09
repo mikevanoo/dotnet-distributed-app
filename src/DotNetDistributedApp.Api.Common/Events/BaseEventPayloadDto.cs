fbe03f2c84e8191f5fa66a719158ae7518a4043d
Mike van Oorschot <mikevanoo@gmail.com>
2026-03-09 15:37:00 +0000

    Remove unused `EventPayloadMap` from `BaseEventPayloadDto`.

--- src/DotNetDistributedApp.Api.Common/Events/BaseEventPayloadDto.cs
+++ src/DotNetDistributedApp.Api.Common/Events/BaseEventPayloadDto.cs
@@ -1,17 +1,10 @@
-﻿using System.Collections.Frozen;
+using System.Collections.Frozen;
+using System.Reflection;
 
 namespace DotNetDistributedApp.Api.Common.Events;
 
 public abstract class BaseEventPayloadDto(string partitionKey)
 {
-    public static readonly FrozenDictionary<string, Type> EventPayloadMap = (
-        new Dictionary<string, Type>
-        {
-            ["simple-event"] = typeof(SimpleEventPayloadDto),
-            ["failing-event"] = typeof(FailingEventPayloadDto),
-        }
-    ).ToFrozenDictionary();
-
     public abstract string EventName { get; }
     public string PartitionKey { get; set; } = partitionKey;
 

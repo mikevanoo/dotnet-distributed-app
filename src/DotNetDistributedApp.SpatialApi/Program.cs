14d9c702b87b5bec43af1201b59f2b531a615e2e
Mike van Oorschot <mikevanoo@gmail.com>
2026-03-09 10:45:00 +0000

    Standardize logging behavior by setting `Environment.ExitCode` on fatal errors and switching to `CloseAndFlushAsync` for graceful log closure.

--- src/DotNetDistributedApp.SpatialApi/Program.cs
+++ src/DotNetDistributedApp.SpatialApi/Program.cs
@@ -55,8 +55,9 @@ try
 catch (Exception ex)
 {
     Log.Fatal(ex, "Application terminated unexpectedly");
+    Environment.ExitCode = 1;
 }
 finally
 {
-    Log.CloseAndFlush();
+    await Log.CloseAndFlushAsync();
 }
